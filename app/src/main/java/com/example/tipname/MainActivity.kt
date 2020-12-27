cdec0c729691f65b4e1d1fefbe9da3d4e00965a8
Mukund Gururani <mukund.gururani@kelltontech.org>
2020-12-27 08:46:11 +0530

    Added dark theme and material design

--- app/src/main/java/com/example/tipname/MainActivity.kt
+++ app/src/main/java/com/example/tipname/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tipname
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tipname.databinding.ActivityMainBinding
@@ -14,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfService.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
@@ -40,5 +45,15 @@ class MainActivity : AppCompatActivity() {
     private fun displayTipAmount(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
